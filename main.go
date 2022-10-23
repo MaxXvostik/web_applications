2b8b31b943f185bef44a33e3e2f35a61585a4e5f
MaxXvostik <xvost-m@mail.ru>
2022-10-23 11:50:07 +0300

    News API

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -14,21 +16,36 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
 }
 
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	params := u.Query()
+	searchQuery := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is: ", searchQuery)
+	fmt.Println("Page is: ", page)
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 
-	// Для подключения css стилей
-	fs := http.FileServer(http.Dir("assets"))
-
-	// Создаем новый мультиплексор HTTP-запросов(Сопоставляет URL-адрес входящих запросов)
 	mux := http.NewServeMux()
-	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	// Этот тип реализует интерфейс Handler путем реализации функции ServeHTTP
 
+	fs := http.FileServer(http.Dir("assets"))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	mux.HandleFunc("/search", searchHandler)
 	mux.HandleFunc("/", indexHandler)
-	//Запускает сервер на порту 3000
 	http.ListenAndServe(":"+port, mux)
 }
